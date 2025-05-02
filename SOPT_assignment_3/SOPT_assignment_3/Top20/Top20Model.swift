10ffc45daab8c5962d550f7107bb79fa4d5fb091
KHW01104 <khw01104@naver.com>
2025-05-02 22:20:17 +0900

    [feat] Top20CollectionView 구현

---
+++ SOPT_assignment_3/SOPT_assignment_3/Top20/Top20Model.swift
@@ -0,0 +1,15 @@
+struct Top20Item {
+    let title: String
+    let imageName: String
+}
+
+struct Top20Model {
+    static let dummy: [Top20Item] = [
+        Top20Item(title: "슬기로운전공의생활", imageName: "tving_doctor"),
+        Top20Item(title: "하트시그널", imageName: "tving_heartsignal"),
+        Top20Item(title: "콩콩밥밥", imageName: "tving_kongkong"),
+        Top20Item(title: "시그널", imageName: "tving_signal"),
+        Top20Item(title: "반지의제왕", imageName: "tving_ring"),
+        Top20Item(title: "해리포터", imageName: "tving_harrypoter")
+    ]
+}
